75086470bb8492bde7d31331eb6d35b70183631d
paulliy <80440049+paulliy@users.noreply.github.com>
2026-02-16 22:39:37 -0500

    added capture button and capture button changing with photo/video mode

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -5,14 +5,19 @@ const bgcolor = ref(true)
 const camcolor = ref(true)
 const bgimg = ref(true)
 const butselect = ref(false)
+const modeselect = ref(false)
 const contcolor = ref(true)
+const capturecolor = ref(true)
+const captureicon = ref(true)
 //const primecolor = ref('var(--photo-primary-container)')
-async function settings(): Promise<void> {
+async function mode(): Promise<void> {
   bgcolor.value = !bgcolor.value
   camcolor.value = !camcolor.value
   bgimg.value = !bgimg.value
-  butselect.value = !butselect.value
   contcolor.value = !contcolor.value
+  modeselect.value = !modeselect.value
+  capturecolor.value = !capturecolor.value
+  captureicon.value = !captureicon.value
   // const mode: boolean = await window.electron.ipcRenderer.invoke('settings')
 }
 const mainBgColor = computed(() => (bgcolor.value ? 'var(--photo-bg)' : 'var(--video-bg)'))
@@ -24,18 +29,30 @@ const mainCamColor = computed(() =>
 const mainContanierColor = computed(() =>
   contcolor.value ? 'var(--photo-primary-container)' : 'var(--video-primary-container)'
 )
+const mainCaptureColor = computed(() =>
+  capturecolor.value ? 'var(--photo-primary)' : 'var(--video-primary)'
+)
 const butborder = computed(() => (butselect.value ? '15px' : '30px'))
+const modeborder = computed(() => (modeselect.value ? '15px' : '30px'))
+const modetext = computed(() => (modeselect.value ? 'Video' : 'Photo'))
+const maincaptureicon = computed(() => (captureicon.value ? '/cameraF.svg' : '/videoF.svg'))
 </script>
 
 <template>
   <main>
     <div class="controls">
-      <button type="button" class="icon-button" aria-label="Button" @click="settings">
+      <button type="button" class="text-button" aria-label="Mode button" @click="mode">
+        {{ modetext }}
+      </button>
+      <button type="button" class="capture" aria-label="Capture button" @click="">
+        <img :src="maincaptureicon" alt="capture button" />
+      </button>
+      <button type="button" class="icon-button" aria-label="Settings Button" @click="">
         <img src="/rightchevronphoto.svg" alt="left chevron" />
       </button>
     </div>
     <div class="box">
-      <img src="/folder.svg" />
+      <img src="/maincamoff.svg" />
     </div>
   </main>
 </template>
@@ -60,13 +77,21 @@ main {
   border-width: 25px;
   border-style: solid;
   border-color: v-bind(mainCamColor);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .controls {
   height: 211px;
-  width: 341px;
+  width: 100vw;
   margin: auto;
-  align-content: center;
+  align-items: center;
   background-color: transparent;
+  display: flex;
+  flex-direction: horizontal;
+  justify-content: center;
+  position: relative;
+  column-gap: 10px;
 }
 .icon-button {
   background-color: v-bind(mainContanierColor);
@@ -79,5 +104,25 @@ main {
 .icon-button img {
   width: 100%;
   height: 100%;
+}
+.text-button {
+  background-color: v-bind(mainContanierColor);
+  border-radius: v-bind(modeborder);
+  padding: 0;
+  cursor: pointer;
+  font-size: 20px;
+  width: 130px;
+  height: 60px;
+  font-family: var(--font-google-sans-flex);
+}
+.capture {
+  background-color: v-bind(mainCaptureColor);
+  border-color: v-bind(mainContanierColor);
+  border-style: solid;
+  border-width: 10px;
+  border-radius: 75px;
+  cursor: pointer;
+  width: 141px;
+  height: 141px;
 }
 </style>
